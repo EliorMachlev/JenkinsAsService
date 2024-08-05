8f6b3784d14757ab5dea0bf21059eb81ecdadc29
EliorMachlev <elior.2100@gmail.com>
2024-08-05 15:03:01 +0300

    Implementing Stop Logic
    Fixed:
    Stop Logic
    Wrong variable calls
    
    Signed-off-by: EliorMachlev <elior.2100@gmail.com>

--- Code/JenkinsAsService.ps1
+++ Code/JenkinsAsService.ps1
@@ -17,8 +17,8 @@ function Invoke-WriteLog {
     $UniqeProcessID = ([System.Diagnostics.Process]::GetCurrentProcess()).ID
     $DateTime = (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
     $Message = "$UniqeProcessID | $DateTime | $LogType | $LogString"
-    Write-Output -InputObject $Message 
-    "$Message" *>>"$LogPath"
+    Write-Information -MessageData  $Message -Tags '1,2'
+    Add-Content -Path "$LogPath" -Value $Message -Force
     return $null
 }
 
@@ -91,7 +91,6 @@ Function Test-TCPConnection {
     
 }
 
-
 Function Invoke-DeafultXML {
     Param (
         [string]
@@ -139,101 +138,117 @@ Function Invoke-DeafultXML {
     }
 }
 
+$InformationPreference = 'Continue'
 
 
-##Check if Windows OS
-if (([System.Environment]::OSVersion.Platform) -ne 'Win32NT' ) { exit }
+TRY {
+    ##Check if Windows OS
+    if (([System.Environment]::OSVersion.Platform) -ne 'Win32NT' ) { exit }
 
-$JenkinsPath = Get-ScriptPath
-$XMLPath = "$JenkinsPath\JenkinsAsService.xml"
-$AgentLog = "$JenkinsPath\agent.log"
+    $JenkinsPath = Get-ScriptPath
+    $XMLPath = "$JenkinsPath\JenkinsAsService.xml"
+    $AgentLog = "$JenkinsPath\agent.log"
 
 
 
-#Read Settings File
-if (Test-Path -Path $XMLPath -PathType 'Leaf') {
-    $Content = (Get-Content -Path $XMLPath -Force)
-    if (($null, '') -contains $Content) {
-        Invoke-DeafultXML -Path $JenkinsPath  -LogPath $AgentLog
-        Invoke-WriteLog -LogPath $AgentLog -LogString 'Please Fill in the XML Values and re-run the Service' -LogType 'Warning'
-        exit    
+    #Read Settings File
+    if (Test-Path -Path $XMLPath -PathType 'Leaf') {
+        $Content = (Get-Content -Path $XMLPath -Force)
+        if (($null, '') -contains $Content) {
+            Invoke-DeafultXML -Path $JenkinsPath  -LogPath $AgentLog
+            Invoke-WriteLog -LogPath $AgentLog -LogString 'Please Fill in the XML Values and re-run the Service' -LogType 'Warning'
+            exit    
+        }
+        else {
+            [xml]$XML = $Content
+
+            $Settings = Convert-XMLtoPSObject -XML ($XML.Objects.Object.property)
+
+        }
     }
     else {
-        [xml]$XML = $Content
-
-        $Settings = Convert-XMLtoPSObject -XML ($XML.Objects.Object.property)
-
+        Invoke-DeafultXML -Path $JenkinsPath
+        Write-Warning -Message 'Please Fill in the XML Values and re-run the Service'
+        exit
     }
-}
-else {
-    Invoke-DeafultXML -Path $JenkinsPath
-    Write-Warning -Message 'Please Fill in the XML Values and re-run the Service'
-    exit
-}
 
 
 
-#Set Settings as Variables
-foreach ($Setting in $Settings) {
-    $Name = $Setting.Name
-    $Value = $Setting.Value
-    $Mandatory = $Setting.Mandatory
-    if (($Setting.Mandatory -eq $true) -and (($null, '') -contains $Setting.Value)) {
-        Invoke-WriteLog -LogPath $AgentLog -LogString  "Jenkins Failed to run: '$Name' is a mandatory field." -LogType 'Error'
-        Exit
+    #Set Settings as Variables
+    foreach ($Setting in $Settings) {
+        $Name = $Setting.Name
+        $Value = $Setting.Value
+        $Mandatory = $Setting.Mandatory
+        if (($Setting.Mandatory -eq $true) -and (($null, '') -contains $Setting.Value)) {
+            Invoke-WriteLog -LogPath $AgentLog -LogString  "Jenkins Failed to run: '$Name' is a mandatory field." -LogType 'Error'
+            Exit
+        }
+        Set-Variable -Name $Setting.Name -Value $Setting.Value -Force
     }
-    Set-Variable -Name $Setting.Name -Value $Setting.Value -Force
-}
 
-if (('', $null) -contains $AgentName) {
-    $AgentName = [System.Net.Dns]::GetHostName()
-    Invoke-WriteLog -LogPath $AgentLog -LogString  "'AgentName' is empty. Using '$AgentName' for this session" -LogType 'Information'
-}
-
-if ((('', $null) -contains $JavaPath) -or (-not (Test-Path -Path "$JavaPath\java.exe" -PathType 'Leaf'))) {
-    Invoke-WriteLog -LogPath $AgentLog -LogString  "'JavaPath' is empty. Looking for JAVA_HOME Environment Variable" -LogType 'Information'
-    if (Test-Path -Path (${env:JAVA_HOME} + '\java.exe') -PathType 'Leaf') {
-        $JavaPath = ${env:JAVA_HOME}
-        Invoke-WriteLog -LogPath $AgentLog -LogString  "JAVA_HOME Environment Variable Found. JavaPAth='$JavaPath' in this session" -LogType 'Information'
-
+    if (('', $null) -contains $AgentName) {
+        $AgentName = [System.Net.Dns]::GetHostName()
+        Invoke-WriteLog -LogPath $AgentLog -LogString  "'AgentName' is empty. Using '$AgentName' for this session" -LogType 'Warning'
     }
-    else {
-        Invoke-WriteLog -LogPath $AgentLog -LogString  "'JavaPath' Value and 'JAVA_HOME' Environment Variable are empty or missing" -LogType 'Error'
+
+    if ((('', $null) -contains $JavaPath) -or (-not (Test-Path -Path "$JavaPath\java.exe" -PathType 'Leaf'))) {
+        Invoke-WriteLog -LogPath $AgentLog -LogString  "'JavaPath' is empty. Looking for JAVA_HOME Environment Variable" -LogType 'Information'
+        if (Test-Path -Path (${env:JAVA_HOME} + '\java.exe') -PathType 'Leaf') {
+            $JavaPath = ${env:JAVA_HOME}
+            Invoke-WriteLog -LogPath $AgentLog -LogString  "JAVA_HOME Environment Variable Found. JavaPAth='$JavaPath' in this session" -LogType 'Information'
+
+        }
+        else {
+            Invoke-WriteLog -LogPath $AgentLog -LogString  "'JavaPath' Value and 'JAVA_HOME' Environment Variable are empty or missing" -LogType 'Error'
+            exit
+        }
+    }
+    Else {
+        if (-not (Test-Path -Path "$JavaPath\java.exe" -PathType Leaf)) {
+            Invoke-WriteLog -LogPath $AgentLog -LogString  "Issue with 'JavaPath': The Path provided ($JavaPath) Does not exist or it does not contain a java.exe executable" -LogType 'Error'
+            exit
+        }
+    }
+
+    ##Check Jenkins URL and Port communication
+    $Jenkins = $jenkinsURL.TrimStart('http://').TrimStart('https://')
+    $JenkinsDomain = $Jenkins.Substring(0, ($Jenkins.IndexOf(":")))
+    $JenkinsPort = $Jenkins.Substring(( $Jenkins.IndexOf(":") + 1 ))
+
+    if ((Test-TCPConnection -address $JenkinsDomain -port $JenkinsPort -Quite) -ne $true) {
+        Invoke-WriteLog -LogPath $AgentLog -LogString  "Issue with 'jenkinsURL': Unable to communicate with '$JenkinsDomain' over Port '$JenkinsPort'" -LogType 'Error'
+        exit
+    }
+
+
+    ##Cleaning Junk Variables From Memory
+    Remove-Variable -Name @('Content', 'Settings', 'XML', 'XMLPath', 'Jenkins', 'JenkinsDomain', 'JenkinsPort') -Force -ErrorAction 'SilentlyContinue'
+
+
+    #Set TLS Security. TLS 1.2 and above. Higher to Lower TLS.
+    [enum]::GetValues('Net.SecurityProtocolType') | Where-Object -FilterScript { $_ -ge 'Tls12' } | Sort-Object -Descending -Unique | ForEach-Object {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+    }
+
+
+    #Reciving Agent.Jar from Jenkins
+    Invoke-WriteLog -LogPath $AgentLog -LogString 'Attepting to establish connection with Jenkins' -LogType 'Information'
+    Invoke-WebRequest -Uri "$JenkinsURL/jnlpJars/agent.jar" -OutFile "$JenkinsPath\agent.jar" -UseBasicParsing -Method 'Get' *>>"$AgentLog"
+
+    #Running The Agent
+    Invoke-WriteLog -LogPath $AgentLog -LogString 'Attepting to start Jenkins Agent' -LogType 'Information'
+    $AgentProcess = Start-Process -FilePath "`"$JavaPath\java.exe`" -jar `"$JenkinsPath\agent.jar`" -url `"$JenkinsURL/`" -secret `"$AgentSecret`" -name `"$AgentName`" -workDir `"$JenkinsPath`"" -WindowStyle 'Hidden' -RedirectStandardError "$AgentLog"  -PassThru
+    $null = Wait-Process -Id $AgentProcess.Id
+}
+
+Catch {
+    Invoke-WriteLog -LogPath "$AgentLog" -LogString ($_ | Convert-String) -LogType 'Error'
+}
+
+Finally {
+    $StopResponse = Stop-Process $AgentProcess.Id -Force -PassThru
+    if ($StopResponse.Id -eq $AgentProcess.Id) {
+        Invoke-WriteLog -LogPath $AgentLog -LogString ("Jenkins Agent (Process ID " + $AgentProcess.Id + ") Stopped.") -LogType 'Warning'
         exit
     }
 }
-Else {
-    if (-not (Test-Path -Path "$JavaPath\java.exe" -PathType Leaf)) {
-        Invoke-WriteLog -LogPath $AgentLog -LogString  "Issue with 'JavaPath': The Path provided ($JavaPath) Does not exist or it does not contain a java.exe executable" -LogType 'Error'
-        exit
-    }
-}
-
-##Check Jenkins URL and Port communication
-$Jenkins = $jenkinsURL.TrimStart('http://').TrimStart('https://')
-$JenkinsDomain = $Jenkins.Substring(0, ($Jenkins.IndexOf(":")))
-$JenkinsPort = $Jenkins.Substring(( $Jenkins.IndexOf(":") + 1 ))
-
-if ((Test-TCPConnection -address $JenkinsDomain -port $JenkinsPort -Quite) -ne $true) {
-    Invoke-WriteLog -LogPath $AgentLog -LogString  "Issue with 'jenkinsURL': Unable to communicate with '$JenkinsDomain' over Port '$JenkinsPort'" -LogType 'Error'
-    exit
-}
-
-
-##Cleaning Junk Variables From Memory
-Remove-Variable -Name @('Content', 'Settings', 'XML', 'XMLPath', 'Jenkins', 'JenkinsDomain', 'JenkinsPort') -Force -ErrorAction 'SilentlyContinue'
-
-
-#Set TLS Security. TLS 1.2 and above. Higher to Lower TLS.
-[enum]::GetValues('Net.SecurityProtocolType') | Where-Object -FilterScript { $_ -ge 'Tls12' } | Sort-Object -Descending -Unique | ForEach-Object {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
-}
-
-
-#Reciving Agent.Jar from Jenkins
-Invoke-WriteLog -LogPath $AgentLog -LogString 'Attepting to establish connection with Jenkins' -LogType 'Information'
-Invoke-WebRequest -Uri "$JenkinsURL/jnlpJars/agent.jar" -OutFile "$JenkinsPath\agent.jar" -UseBasicParsing -Method 'Get' *>>"$AgentLog"
-
-#Running The Agent
-Invoke-WriteLog -LogPath $AgentLog -LogString 'Attepting to start Jenkins Agent' -LogType 'Information'
-& "$JavaPath\java.exe" -jar "$JenkinsPath\agent.jar" -url "$JenkinsURL/" -secret "$AgentSecret" -name "$AgentName" -workDir "$JenkinsPath" *>>"$AgentLog"
